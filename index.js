faa89e00c4c4c165e43e37b6015ecab1d69ddd93
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-06-16 11:44:47 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    function namedFunction(){
+        console.log()
+    }
+    return namedFunction
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log()
+    }
+}
